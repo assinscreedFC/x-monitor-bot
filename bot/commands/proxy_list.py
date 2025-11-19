193f9eab4471adddf20f06e504bffb488baed99f
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2025-11-19 15:23:50 +0100

    version chinois

--- bot/commands/proxy_list.py
+++ bot/commands/proxy_list.py
@@ -46,8 +46,7 @@ async def send_long_message(update: Update, text: str, parse_mode: str = None):
 
         if i > 0:
             # Le header est en chinois
-            header = rf"**(继续 \- 第 {i + 1}/{len(parts)} 部分)**\n"
-
+            header = rf"\[继续 \- 第 {i + 1}/{len(parts)} 部分\]\n"
         await update.message.reply_text(header + part, parse_mode=parse_mode, reply_markup=reply_markup)
 
         # Petite pause pour éviter le flood
